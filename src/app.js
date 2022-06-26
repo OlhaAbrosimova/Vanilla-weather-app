d23b00bf2dcf419dafd9e6b5e8f77a3916079daf
Olha Abrosimova <abrosimova2020@ukr.net>
2022-06-26 13:34:49 +0300

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+    if (hours < 10) {
+			hours = `0${hours}`;
+		}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+
+	return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
 	let temperatureElement = document.querySelector("#base-temperature");
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,16 +34,8 @@ function displayTemperature(response) {
 	humidityElement.innerHTML = response.data.main.humidity;
 	let windElement = document.querySelector("#wind-speed");
 	windElement.innerHTML = response.data.wind.speed.toFixed(1);
-
-	// let day = [
-	// 	"Sunday",
-	// 	"Monday",
-	// 	"Tuesday",
-	// 	"Wednesday",
-	// 	"Thursday",
-	// 	"Friday",
-	// 	"Saturday",
-	// ];
+	let dateElement = document.querySelector("#date");
+	dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "a2e58143d5353df7726302c2856b0fb6";
