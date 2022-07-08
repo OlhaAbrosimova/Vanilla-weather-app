f1700424539f6c3bd74a343818dd0b4fc7c8b235
Olha Abrosimova <abrosimova2020@ukr.net>
2022-07-08 19:37:24 +0300

    style changes in central block

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ function formatDay(timestamp) {
 	return days[day];
 }
 function displayForecast(response) {
-	console.log(response.data.daily);
+	
 	let forecast = response.data.daily;
 	let forecastElement = document.querySelector("#forecast");
 
